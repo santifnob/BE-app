488052761dea10e90c2fdc3ec176a613eded0187
Gonza <constante.gonzalo2@gmail.com>
2025-08-04 16:05:07 -0300

    Add files via upload
    Correcting code syntax and normalizing text outputs.

--- src/licencia/licencia.controller.ts
+++ src/licencia/licencia.controller.ts
@@ -5,11 +5,7 @@ import { Conductor } from '../conductor/conductor.entity.js'
 
 const em = orm.em
 
-function sanitizeLicenciaInput (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): void {
+function sanitizeLicenciaInput (req: Request, res: Response, next: NextFunction): void {
   req.body.sanitizedInput = {
     fechaVencimiento: req.body.fechaVencimiento,
     fechaHecho: req.body.fechaVencimiento,
@@ -28,28 +24,20 @@ function sanitizeLicenciaInput (
 
 async function findAll (req: Request, res: Response): Promise<void> {
   try {
-    const licencias = await em.find(
-      Licencia,
-      {},
-      { populate: ['conductor'] }
-    )
-    res.status(200).json({ message: 'Licencias encontradas: ', data: licencias })
+    const licencias = await em.find(Licencia, {}, { populate: ['conductor'] })
+    res.status(200).json({ message: 'Listado de las licencias de conductor: ', data: licencias })
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: 'Error al obtener el listado de las licencias de conductor', error: error.message })
   }
 }
 
 async function findOne (req: Request, res: Response): Promise<void> {
   try {
     const id = Number.parseInt(req.params.id)
-    const licencia = await em.findOneOrFail(
-      Licencia,
-      { id },
-      { populate: ['conductor'] }
-    )
-    res.status(200).json({ message: 'Licencia encontrada: ', data: licencia })
+    const licencia = await em.findOneOrFail(Licencia, { id }, { populate: ['conductor'] })
+    res.status(200).json({ message: 'La "Licencia de conductor" ha sido encontrada: ', data: licencia })
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: 'Error al obtener la "Licencia de conductor"', error: error.message })
   }
 }
 
@@ -60,9 +48,9 @@ async function add (req: Request, res: Response): Promise<void> {
     req.body.sanitizedInput.conductor = conductor
     const licencia = em.create(Licencia, req.body.sanitizedInput)
     await em.flush()
-    res.status(201).json({ message: 'Licencia creada', data: licencia })
+    res.status(201).json({ message: 'La "Licencia de conductor" ha sido creada con exito: ', data: licencia })
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: 'Error al agregar la "Licencia de conductor"', error: error.message })
   }
 }
 
@@ -80,11 +68,9 @@ async function update (req: Request, res: Response): Promise<void> {
     em.assign(licenciaToUpdate, req.body.sanitizedInput)
     await em.flush()
     Object.assign(req.body.sanitizedInput, licenciaToUpdate) // esto porque sino licenciaUpdated no muestra los datos del conductor
-    res
-      .status(200)
-      .json({ message: 'Licencia actualizada', data: req.body.sanitizedInput })
+    res.status(200).json({ message: 'La "Licencia de conductor" ha sido actualizada con exito: ', data: licenciaToUpdate })
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: 'Error al actualizar la "Licencia de conductor"', error: error.message })
   }
 }
 
@@ -93,8 +79,9 @@ async function remove (req: Request, res: Response): Promise<void> {
     const id = Number.parseInt(req.params.id)
     const licencia = em.getReference(Licencia, id)
     await em.removeAndFlush(licencia)
+    res.status(200).json({ message: 'La "Licencia de conductor" ha sido eliminada con exito: ', data: licencia })
   } catch (error: any) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: 'Error al eliminar la "Licencia de conductor"', error: error.message })
   }
 }
 
