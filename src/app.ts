1829d46f9704d87a86067213ed95d9ff00d2ce4f
Santiago Ferrero <santifnob@gmail.com>
2025-08-12 15:12:38 -0300

    Add cookie-parse dependency and fix minor bugs

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata'
 import express from 'express'
 import cors from 'cors'
 import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 import { trenRouter } from './tren/tren.routes.js'
 import { orm, syncSchema } from './shared/db/orm.js'
 import { RequestContext } from '@mikro-orm/core'
@@ -19,14 +20,14 @@ import { authenticateToken } from './middlewares/authMiddlewares.js'
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser()) // Middleware para parsear las cookies entrantes
 
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['POST', 'GET', 'DELETE', 'PUT'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // Permite enviar cookies,
 }))
-
-app.options('*', cors())
 
 app.use((req, res, next) => {
   RequestContext.create(orm.em, next)
@@ -63,10 +64,9 @@ app.post('/api/auth/login', (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: false, // por ahora se mantiene sin https
-      sameSite: 'strict',
-      maxAge: 3600000 // 1 hora
+      sameSite: 'lax'
     })
-
+    
     return res.status(200).json({ message: 'Login exitoso', userData: { id: user.id, role: user.role } })
   }
 
@@ -74,7 +74,7 @@ app.post('/api/auth/login', (req, res) => {
 })
 
 app.get('/api/auth/check', authenticateToken, (req, res) => {
-  res.status(200).json({ message: 'Token valido', userData: req.body.user}) // Validacion justamente hecha en el middleware authenticateToken
+  return res.status(200).json({ message: 'Token valido', userData: req.body.user}) // Validacion justamente hecha en el middleware authenticateToken
 })
 
 app.use((_, res) => {
--- src/middlewares/authMiddlewares.ts
+++ src/middlewares/authMiddlewares.ts
@@ -1,16 +1,16 @@
 import { secretKey } from '../app.js'
 import { NextFunction, Request, Response } from 'express'
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken' 
 
-export function authenticateToken (req: Request, res: Response, next: NextFunction): void {
+export function authenticateToken (req: Request, res: Response, next: NextFunction): Response | undefined {
   const token = req.cookies.token
 
-  if (token === undefined) {
-    res.status(401).json({ message: 'Token no proporcionado' })
+  if (!token) {
+    return res.status(401).send('Token no proporcionado')
   }
 
   jwt.verify(token, secretKey, (err: jwt.VerifyErrors | null, payload: string | jwt.JwtPayload | undefined) => {
-    if (err != null) return res.status(403).json({ message: 'Token invalido' })
+    if (err != null) return res.status(403).send('Token invalido')
 
     req.body.user = payload // {role , userId}
 
@@ -19,9 +19,9 @@ export function authenticateToken (req: Request, res: Response, next: NextFuncti
 }
 
 export function authorizeRole(role: string): Function {
-  return (req: Request, res: Response, next: NextFunction): void => {
+  return (req: Request, res: Response, next: NextFunction): Response | undefined => {
     if(req.body.user.role !== role) {
-      res.status(403).json({message: 'No tienes permisos para acceder a este recurso'})
+     return res.status(403).json({message: 'No tienes permisos para acceder a este recurso'})
     }
     next()
   }
